317e6229bfbbe3245bdee15217c29f8ba174827d
Teemu Kaukoranta <teemu.kaukoranta@gmail.com>
2018-01-09 13:58:46 +0200

    Remove thefuck from config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -27,7 +27,7 @@ source ~/.tacklebox/tacklebox.fish
 
 # thefuck
 # eval (thefuck --alias | tr '\n' ';')
-thefuck --alias | source 
+# thefuck --alias | source 
 
 # iTerm2 Shell Integration
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
