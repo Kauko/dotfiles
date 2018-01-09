2b87d2b265dca094543af1b197969c761d4a4932
Teemu Kaukoranta <teemu.kaukoranta@gmail.com>
2018-01-09 12:52:32 +0200

    Remove anaconda, composer from fish.config

--- fish/config.fish
+++ fish/config.fish
@@ -11,13 +11,13 @@ set -x -g PATH (brew --prefix findutils)/libexec/gnubin $PATH
 # set -x -g MANPATH (brew --prefix findutils)/libexec/gnuman $MANPATH
 
 # User bin folder
-set -x -g PATH ~/anaconda3/bin $PATH ~/bin /usr/local/sbin
+# set -x -g PATH ~/anaconda3/bin $PATH ~/bin /usr/local/sbin
 
 # Composer
-set -x -g PATH ~/.composer/vendor/bin $PATH
+# set -x -g PATH ~/.composer/vendor/bin $PATH
 
 # Source conda.fish
-source (conda info --root)/etc/fish/conf.d/conda.fish
+# source (conda info --root)/etc/fish/conf.d/conda.fish
 
 ## Teemun lisäämät
 # Tacklebox
